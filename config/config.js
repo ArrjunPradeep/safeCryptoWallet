e4dd03735770070b87a9f5b89b41a141fd5ee049
Arju <iaat@pop-os.localdomain>
2021-09-06 20:09:23 +0530

    validation_added

--- config/config.js
+++ config/config.js
@@ -16,6 +16,7 @@ module.exports = {
         gasLimit: "50000", //50000
         provider: "https://data-seed-prebsc-2-s3.binance.org:8545/",
         websocket: "wss://bsc.getblock.io/testnet/?api_key=b6c78cbb-4bf5-4719-838f-11e3cf7604f8",//"wss://bsc.getblock.io/testnet/?api_key=7f919aac-9d46-49f6-8dc9-453d3a9471a6",
-        initialBlock: '0'
+        initialBlock: '0',
+        apiKey: "L4gTDYc4af=2n=Kj"
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -9,83 +9,121 @@ const walletsModel = require("../models/wallets");
 const tokensModel = require("../models/tokens");
 const transactionsModel = require("../models/transactions");
 const constants = require("../constants/constants");
+const config = require("../config/config");
+const body = require("express-validator").body;
+const header = require("express-validator").header;
+const query = require("express-validator").query;
+const validationResult = require("express-validator").validationResult;
+
+const validate = (routeName) => {
+  switch (routeName) {
+    case "createWallet":
+      return [
+        header("x-api-key").exists().equals(config.wallet.apiKey),
+        body("email").exists().isString().notEmpty().isEmail(),
+      ];
+    case "send":
+      return [
+        header("x-api-key").exists().equals(config.wallet.apiKey),
+        body("email").exists().isString().notEmpty().isEmail(),
+        body("crypto").exists().isString().notEmpty(),
+        body("receiver").exists().isString().notEmpty(),
+        body("amount").exists().isString().notEmpty(),
+      ];
+    case "validateTransaction":
+      return [
+        header("x-api-key").exists().equals(config.wallet.apiKey),
+        body("email").exists().isString().notEmpty().isEmail(),
+        body("crypto").exists().isString().notEmpty(),
+        body("amount").exists().isString().notEmpty(),
+      ];
+    case "transactionHistory":
+      return [
+        header("x-api-key").exists().equals(config.wallet.apiKey),
+        header("x-api-key").exists(),
+        query("email").exists().isString().notEmpty().isEmail(),
+      ];
+    case "user":
+      return [
+        header("x-api-key").exists().equals(config.wallet.apiKey),
+        query("email").exists().isString().notEmpty().isEmail(),
+      ];
+  }
+};
 
 // MIDDLEWARE API-KEY AUTHENTICATION
-router.use(jwt_lib.router);
-
-// ACTIVATE ACCOUNT
-router.post("/activateAccount", async (req, res, next) => {
-  try {
-
-    let token = "srfsdgf";
-
-    return res.status(200).send({
-      status: true,
-      token: token,
-    });
-  } catch (error) {
-    console.log("ACTIVATE ACCOUNT :: ", error);
-
-    return res.status(500).send({
-      status: false,
-      message: "Internal Server Error",
-    });
-  }
-});
+// router.use(jwt_lib.router);
 
 // CREATE NEW WALLETS - BNB & TOKENS
-router.get("/createWallet", async (req, res, next) => {
-  try {
-    let { email, password } = req.body;
+router.post(
+  "/createWallet",
+  validate("createWallet"),
+  async (req, res, next) => {
+    try {
+      let errors = validationResult(req);
+      if (errors.isEmpty() == false) {
+        res.status(412).send({
+          status: false,
+          message: "Validation Failed",
+          error: errors,
+        });
+        return;
+      }
 
-    let wallet_data = await createWallet(email, req.body);
+      let { email, password } = req.body;
 
-    // let token = await jwt_lib.generateToken();
+      let wallet_data = await createWallet(email, req.body);
 
-    if(wallet_data.status == true) {
-      return res.status(200).send({
-        status: true,
-        // token: token,
-        message: "success"
-      });
-    }
-    else{
-      return res.status(400).send({
+      // let token = await jwt_lib.generateToken();
+
+      if (wallet_data.status == true) {
+        return res.status(200).send({
+          status: true,
+          // token: token,
+          message: "success",
+        });
+      } else {
+        return res.status(400).send({
+          status: false,
+          message: wallet_data.message,
+        });
+      }
+    } catch (error) {
+      console.log("ERROR :: ", error);
+
+      return res.status(500).send({
+        message: "Internal Server Error",
         status: false,
-        message: wallet_data.message,
       });
     }
-
-
-
-  } catch (error) {
-    console.log("ERROR :: ", error);
-
-    return res.status(500).send({
-      message: "Internal Server Error",
-      status: false,
-    });
   }
-});
+);
 
-// SEND COIN & TOKEN 
-router.post("/send", async (req, res, next) => {
+// SEND COIN & TOKEN
+router.post("/send", validate("send"), async (req, res, next) => {
   try {
+    let errors = validationResult(req);
+    if (errors.isEmpty() == false) {
+      res.status(412).send({
+        status: false,
+        message: "Validation Failed",
+        error: errors,
+      });
+      return;
+    }
 
     let { email, crypto, receiver, amount } = req.body;
 
     let promises = await Promise.all([
-      walletsModel.findOne({email:email}).lean().exec(),
-      tokensModel.findOne({symbol:crypto}).lean().exec()
-    ])
+      walletsModel.findOne({ email: email }).lean().exec(),
+      tokensModel.findOne({ symbol: crypto }).lean().exec(),
+    ]);
 
-    if(promises[1] == null && crypto != 'BNB') {
-
+    if (promises[1] == null && crypto != "BNB") {
       return res.status(401).send({
-        status:false,
-        message:"Invalid Token"
-      })
-
+        status: false,
+        message: "Invalid Token",
+      });
     }
 
     let balance = promises[0][crypto.toLowerCase()].balance;
@@ -96,9 +134,8 @@ router.post("/send", async (req, res, next) => {
       balance,
       amount
     );
-    
-    if(checkBalance.status) {
-     
+
+    if (checkBalance.status) {
       await transactionsModel.create({
         email: email,
         ref: promises[0].id,
@@ -107,181 +144,194 @@ router.post("/send", async (req, res, next) => {
         source: crypto.toLowerCase(),
         target: crypto.toLowerCase(),
         sourceAmount: amount,
-        targetAmount:amount,
+        targetAmount: amount,
         type: "send",
         currency: crypto.toLowerCase(),
-        hash: '',
+        hash: "",
         status: constants.TXNS.IN_QUEUE,
-        error: 'nil',
+        error: "nil",
         gasLimit: checkBalance.gasLimit,
         gasPrice: checkBalance.gasPrice,
         fee: checkBalance.fee,
-        timestamp: String(new Date().getTime())
-      })
+        timestamp: String(new Date().getTime()),
+      });
 
       return res.status(200).send({
         status: true,
-        message: "Transaction Initiated"
+        message: "Transaction Initiated",
       });
-
-    }
-    else {
-
+    } else {
       return res.status(400).send({
         status: false,
-        message: "Insufficient Balance"
+        message: "Insufficient Balance",
       });
-
     }
-
   } catch (error) {
-
-    console.log("ERROR::SEND::",error);
+    console.log("ERROR::SEND::", error);
 
     return res.status(500).send({
       message: "Internal Server Error",
       status: false,
     });
   }
-})
+});
 
-// VALIDATE THE TRANSACTION 
-router.post("/validateTransaction", async(req, res, next) => {
+// VALIDATE THE TRANSACTION
+router.post(
+  "/validateTransaction",
+  validate("validateTransaction"),
+  async (req, res, next) => {
+    try {
+      let errors = validationResult(req);
+      if (errors.isEmpty() == false) {
+        res.status(412).send({
+          status: false,
+          message: "Validation Failed",
+          error: errors,
+        });
+        return;
+      }
 
-  try {
-    
-    let { email, crypto, amount } = req.body;
+      let { email, crypto, amount } = req.body;
 
-    let promises = await Promise.all([
-      walletsModel.findOne({email:email}).lean().exec(),
-      tokensModel.findOne({symbol:crypto}).lean().exec()
-    ])
+      let promises = await Promise.all([
+        walletsModel.findOne({ email: email }).lean().exec(),
+        tokensModel.findOne({ symbol: crypto }).lean().exec(),
+      ]);
 
-    if(promises[1] == null && crypto != 'BNB') {
+      if (promises[1] == null && crypto != "BNB") {
+        return res.status(401).send({
+          status: false,
+          message: "Invalid Token",
+        });
+      }
 
-      return res.status(401).send({
-        status:false,
-        message:"Invalid Token"
-      })
+      let balance = promises[0][crypto.toLowerCase()].balance;
 
-    }
+      let checkBalance = await ethereum_lib.validateTransaction(
+        crypto,
+        balance,
+        amount
+      );
 
-    let balance = promises[0][crypto.toLowerCase()].balance;
+      if (checkBalance.status) {
+        return res.status(200).send({
+          status: true,
+          message: "CONFIRM",
+        });
+      } else {
+        return res.status(400).send({
+          status: false,
+          message: "INSUFFICIENT BALANCE",
+        });
+      }
+    } catch (error) {
+      console.log(":: ERROR ::", error);
 
-    let checkBalance = await ethereum_lib.validateTransaction(
-      crypto,
-      balance,
-      amount
-    );
-    
-    if(checkBalance.status) {
-     
-      return res.status(200).send({
-        status: true,
-        message: "CONFIRM"
-      });
-
-    }
-    else {
-
-      return res.status(400).send({
+      return res.status(500).send({
         status: false,
-        message: "INSUFFICIENT BALANCE"
+        message: "Internal Server Error",
       });
-
     }
-
-  } catch (error) {
-    
-    console.log(":: ERROR ::", error);
-
-    return res.status(500).send({
-      status:false,
-      message:"Internal Server Error"
-    })
-
   }
-
-})
+);
 
 // RETREIVE TRANSACTION HISTORY
-router.get("/transactionHistory", async(req,res) => {
-
-  try {
-    
-    let sendTransactions = await transactionsModel.find({email:req.query.email,type:"send"},{
-      source: 1,
-      sourceAmount: 1,
-      targetAmount: 1,
-      type: 1,
-      status: 1,
-      timestamp: 1,
-      to: 1,
-      hash: 1,
-      _id:0
-    }).sort({timestamp:-1}).lean().exec();
-    
-    let receiveTransactions = await transactionsModel.find({email:req.query.email,type:"received"},{
-      source: 1,
-      sourceAmount: 1,
-      targetAmount: 1,
-      type: 1,
-      status: 1,
-      timestamp: 1,
-      from: 1,
-      hash: 1,
-      _id:0
-		}).sort({timestamp:-1}).lean().exec();
-
-    return res.status(200).send({
-      status: true,
-      message: {
-        send:sendTransactions,
-        receive:receiveTransactions
+router.get(
+  "/transactionHistory",
+  validate("transactionHistory"),
+  async (req, res) => {
+    try {
+      let errors = validationResult(req);
+      if (errors.isEmpty() == false) {
+        res.status(412).send({
+          status: false,
+          message: "Validation Failed",
+          error: errors,
+        });
+        return;
       }
-    })
 
+      let sendTransactions = await transactionsModel
+        .find(
+          { email: req.query.email, type: "send" },
+          {
+            source: 1,
+            sourceAmount: 1,
+            targetAmount: 1,
+            type: 1,
+            status: 1,
+            timestamp: 1,
+            to: 1,
+            hash: 1,
+            _id: 0,
+          }
+        )
+        .sort({ timestamp: -1 })
+        .lean()
+        .exec();
 
-  } catch (error) {
-    
-    console.log("::TRANSACTION HISTORY::ERROR::",error);
-    return res.status(500).send({
-      status: false,
-      message: "Internal Server Error"
-    })
+      let receiveTransactions = await transactionsModel
+        .find(
+          { email: req.query.email, type: "received" },
+          {
+            source: 1,
+            sourceAmount: 1,
+            targetAmount: 1,
+            type: 1,
+            status: 1,
+            timestamp: 1,
+            from: 1,
+            hash: 1,
+            _id: 0,
+          }
+        )
+        .sort({ timestamp: -1 })
+        .lean()
+        .exec();
 
+      return res.status(200).send({
+        status: true,
+        message: {
+          send: sendTransactions,
+          receive: receiveTransactions,
+        },
+      });
+    } catch (error) {
+      console.log("::TRANSACTION HISTORY::ERROR::", error);
+      return res.status(500).send({
+        status: false,
+        message: "Internal Server Error",
+      });
+    }
   }
-
-})
+);
 
 // RETREIVE USER DETAILS
-router.get("/user", async ( req, res, next) => {
-
+router.get("/user", async (req, res, next) => {
   try {
-
     let { email } = req.query;
 
     console.log("QUERY::", email);
 
-    let user = await walletsModel.findOne({email:email},{_id:0,id:0,__v:0}).lean().exec();
-    
-    return res.status(200).send({
-      status:true,
-      message:user
-    })
+    let user = await walletsModel
+      .findOne({ email: email }, { _id: 0, id: 0, __v: 0 })
+      .lean()
+      .exec();
 
+    return res.status(200).send({
+      status: true,
+      message: user,
+    });
   } catch (error) {
-    
     console.log("::USER::ERROR::", error);
 
     return res.status(500).send({
-      status:false,
-      message:"Internal Server Error"
-    })
-
+      status: false,
+      message: "Internal Server Error",
+    });
   }
-
-})
+});
 
 // CREATE CRYPTO ADDRESSESS FOR NEW USERS
 const createWallet = async (email, userData) => {
@@ -293,7 +343,6 @@ const createWallet = async (email, userData) => {
     let result = await accountsModel
       .find({ adminLevel: { $nin: ["0"] } })
       .sort({ ref: -1 });
-
 
     if (result.length > 0) {
       console.log("Lastre");
@@ -312,21 +361,21 @@ const createWallet = async (email, userData) => {
 
     let wallet = {
       bnb: ethereum_wallet,
-      bobe: ethereum_wallet
+      bobe: ethereum_wallet,
     };
 
     userData.wallets = wallet;
 
     let block_balance = {
-      bnb: '0',
-      bobe: '0'
-    }
+      bnb: "0",
+      bobe: "0",
+    };
 
     await accountsModel.create({
       email: email,
       password: String(await bcrypt_lib.hash(userData.password)),
       id: email,
-      ref: ref
+      ref: ref,
     });
 
     await walletsModel.create({
@@ -341,53 +390,42 @@ const createWallet = async (email, userData) => {
         balance: block_balance.bobe,
         address: wallet.bobe,
         fee: 0,
-      }
+      },
     });
 
     return {
       status: true,
-      message: "success"
-    }
-
+      message: "success",
+    };
   } catch (error) {
-
-    console.log("ERROR::",error);
+    console.log("ERROR::", error);
 
     return {
       status: false,
-      message: error.code == 11000 ? "Duplicate Entry Found" : "Error"
-    }
-
+      message: error.code == 11000 ? "Duplicate Entry Found" : "Error",
+    };
   }
 };
 
 // CHECK IS EXTERNAL ADDRESS
-const isExternalAddress = async(crypto, address) => {
-
+const isExternalAddress = async (crypto, address) => {
   try {
-    
     let flag = true;
 
-		let account = await walletsModel.find({}).lean().exec();
+    let account = await walletsModel.find({}).lean().exec();
 
-		await account.forEach(async result => {
-      console.log("wetr",result.bnb.address)
-			if (result.wallets[`${crypto.toLowerCase()}.address`] == address) {
-        
-        flag = false
-        console.log("FLAG::",flag);
-
-			}
+    await account.forEach(async (result) => {
+      console.log("wetr", result.bnb.address);
+      if (result.wallets[`${crypto.toLowerCase()}.address`] == address) {
+        flag = false;
+        console.log("FLAG::", flag);
+      }
     });
-    
+
     return flag;
-
   } catch (error) {
-    
-    console.log("ERROR::",error);
-
+    console.log("ERROR::", error);
   }
-
-} 
+};
 
 module.exports = router;
