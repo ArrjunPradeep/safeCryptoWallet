bfa61c4925a334d4a6141ebfb699c7078f6153d1
Arju <iaat@pop-os.localdomain>
2021-10-12 10:03:39 +0530

    deploy_scripts_added

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+
+async function main() {
+
+  const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
+  const nftMarket = await NFTMarket.deploy();
+  await nftMarket.deployed();
+  console.log(":: NFT MARKETPLACE :: ", nftMarket.address);
+
+  const NFT = await hre.ethers.getContractFactory("NFT");
+  const nft = await NFT.deploy(nftMarket.address);
+  await nft.deployed();
+  console.log(":: NFT :: ", nft.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
