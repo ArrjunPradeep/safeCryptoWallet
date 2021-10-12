849aaf4fe3c38efcf207e20f127594e7e1cec73f
Arju <iaat@pop-os.localdomain>
2021-10-12 10:05:40 +0530

    hardhat_config_updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,24 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// npx hardhat verify --network ropsten 0x5FbDB2315678afecb367f032d93F642f64180aa3 "Success" "SCS" 18
+// npx hardhat verify --constructor-args scripts/token.js 0xE79dA49b082D21cfD313104D071cb3856C928d71 --network ropsten
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks:{
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      // gasPrice: 1000000000,
+      accounts:[`0x${PRIVATE_KEY}`]
+    },
+    testnet: {
+      url: "https://data-seed-prebsc-2-s3.binance.org:8545/",
+      chainId: 97,
+      accounts:[`0x${PRIVATE_KEY}`]
+    },
+  }
 };
