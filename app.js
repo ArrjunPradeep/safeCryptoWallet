678060c219a2c36aa5fbfe4dea8f31e37a8269ca
Arjun-Pradeep <arjunpradeepm17@gmail.com>
2021-11-11 16:35:17 +0530

    app.js_updated

--- app.js
+++ app.js
@@ -6,26 +6,35 @@ const mongoose = require("mongoose");
 const config = require("./config/config");
 
 // D A T A B A S E    C O N N E C T I O N  -  M O N G O D B
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
 mongoose.connect(
 
   "mongodb://" +
-    config.db.userName +
-    ":" +
-    config.db.password +
-    "@" +
-    config.db.host +
-    ":" +
-    config.db.port +
-    "/" +
-    config.db.dbName
+  config.db.userName +
+  ":" +
+  config.db.password +
+  "@" +
+  config.db.host +
+  ":" +
+  config.db.port +
+  "/" +
+  config.db.dbName,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  }
+).then(async (res) => {
 
-    ).then(async(res) => {
-
-    console.log(":::::::  MONGODB CONNECTED :::::::");
+  console.log(":::::::  MONGODB CONNECTED :::::::");
 
 }).catch((error) => {
 
-    console.log("::::::: MONGODB NOT CONNECTED :::::::");
+  console.log("::::::: MONGODB NOT CONNECTED :::::::");
 
 })
 
