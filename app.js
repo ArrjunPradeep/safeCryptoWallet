de1f22f910902a5feffa39c916fb5257ccef3741
Arjun-Pradeep <arjunpradeepm17@gmail.com>
2021-11-18 12:54:38 +0530

    wallet_route_updated

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", userRouter);
+app.use("/wallet", userRouter);
 app.use("/admin", adminRouter);
 app.use("/nft", nftRouter);
 
